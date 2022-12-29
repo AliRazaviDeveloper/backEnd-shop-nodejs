aa31623f61a625382440aa838918feb37f5c22c3
AliRazavi <razavi.ali.1998@gmail.com>
2022-12-29 15:25:07 +0330

    feat: morgan logger request

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
 module.exports = (app) => {
+  app.use(morgan());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  app.use(
+    express.static(require("path").join(__dirname, "..", "..", "public"))
+  );
 };
